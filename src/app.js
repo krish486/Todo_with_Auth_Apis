ada023019206f4d0dd05bc2dd84ae61a0687d57c
Krish <khambhatikrish@gmail.com>
2026-05-28 17:29:42 +0530

    auth routes connection with app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 let express = require("express")
+let authRoutes = require("./routes/auth.routes")
 
 let app = express()
+
+
+///---accepting data from client
+app.use(express.json())
+
+
+//--connecting routes with app
+app.use("/auth", authRoutes)
 
 
 
